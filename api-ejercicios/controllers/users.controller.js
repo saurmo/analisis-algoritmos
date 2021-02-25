c8bb9ee9d2516c9cb97c8d5eb7f8577f7bbed378
saurmo <santiago@creativosdigitales.co>
2021-02-25 07:21:32 -0500

    - Conexión a base de datos

--- api-ejercicios/controllers/users.controller.js
+++ api-ejercicios/controllers/users.controller.js
@@ -1,8 +1,15 @@
 const _pg = require("../services/postgres.service");
 
-const getUsers = (req, res) => {
-  //_pg.execute()
-  return res.send(" - Usuarios Controlador");
+const getUsers = async (req, res) => {
+  try {
+    let sql = "SELECT * FROM usuarios";
+    let response_db = await _pg.execute(sql);
+    let rows = response_db.rows;
+    return res.send(rows);
+  } catch (error) {
+    console.error(error);
+    return res.send(error);
+  }
 };
 
 module.exports = { getUsers };
--- api-ejercicios/services/postgres.service.js
+++ api-ejercicios/services/postgres.service.js
@@ -1,7 +1,13 @@
 const pg = require("pg");
 
 const execute = async (sql) => {
-  const client = new pg.Client();
+  const client = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "censo",
+    password: "admin",
+    port: 5432,
+  });
   await client.connect();
   const res = await client.query(sql);
   await client.end();
