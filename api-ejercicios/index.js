d95ea06dd90d4c9990f01f6b72799d464c4b3d2f
saurmo <santiago@creativosdigitales.co>
2021-03-09 07:22:44 -0500

    - CRUD usuarios

--- api-ejercicios/index.js
+++ api-ejercicios/index.js
@@ -4,7 +4,7 @@
 const express = require("express");
 
 const app = express();
-
+app.use(express.json());
 //  INVOCAR LAS RUTAS DE LA CARPETA ROUTERS
 const routers = require("./routers/index");
 app.use(routers);
--- api-ejercicios/routers/index.js
+++ api-ejercicios/routers/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const user_controller = require("../controllers/users.controller");
 
 router.get("/usuarios", user_controller.getUsers);
+router.post("/usuarios", user_controller.saveUser);
+router.delete("/usuarios/:identificacion", user_controller.deleteUser);
+router.put("/usuarios/:identificacion", user_controller.updateUser);
 
 module.exports = router;
