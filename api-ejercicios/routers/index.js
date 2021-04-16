76d10494cfe80900d3b0e4e7e1ed46cdf9636815
saurmo <santiago@creativosdigitales.co>
2021-04-16 16:25:42 -0500

    - Endpoint para consultar un usuario

--- api-ejercicios/routers/index.js
+++ api-ejercicios/routers/index.js
@@ -4,7 +4,9 @@ const user_controller = require("../controllers/users.controller");
 const reports_controller = require("../controllers/reports.controller");
 
 router.get("/reportes/eps", reports_controller.getReportEps);
+
 router.get("/usuarios", user_controller.getUsers);
+router.get("/usuarios/:identificacion", user_controller.getUsers);
 router.post("/usuarios", user_controller.saveUser);
 router.delete("/usuarios/:identificacion", user_controller.deleteUser);
 router.put("/usuarios/:identificacion", user_controller.updateUser);
