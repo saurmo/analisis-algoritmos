435b999d4a0fad3d2aa8186cc60a83ff2f522f4b
saurmo <santiago@creativosdigitales.co>
2021-05-20 07:11:34 -0500

    - Ultimos cambios

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: "API",
+      script: "api-ejercicios/index.js",
+      exec_mode: "fork",
+    },
+  ],
+};
--- js/ejercicios-dane/total-particulares.js
+++ js/ejercicios-dane/total-particulares.js
@@ -3,10 +3,11 @@ const DATA = require("./info.json");
 const ejercicio1 = () => {
   //Implementación 1
   console.time("EJERCICIO 1 - Implementación 1");
+  // for
   let total = 0;
   for (let i = 0; i < DATA.length; i++) {
     const element = DATA[i];
-    const particulares = parseInt(element.particulares);
+    const particulares = parseInt(element.particulares.split(".").join(""));
     if (particulares && !Number.isNaN(particulares)) {
       total += particulares;
     } else {
@@ -16,9 +17,10 @@ const ejercicio1 = () => {
   console.timeEnd("EJERCICIO 1 - Implementación 1");
 
   console.time("EJERCICIO 1 - Implementación 2");
+  // for of
   let total2 = 0;
   for (const element of DATA) {
-    const particulares = parseInt(element.particulares);
+    const particulares = parseInt(element.particulares.split(".").join(""));
     if (particulares && !Number.isNaN(particulares)) {
       total2 += particulares;
     } else {
@@ -28,9 +30,10 @@ const ejercicio1 = () => {
   console.timeEnd("EJERCICIO 1 - Implementación 2");
 
   console.time("EJERCICIO 1 - Implementación 3");
+  // forEach
   let total3 = 0;
   DATA.forEach((element) => {
-    const particulares = parseInt(element.particulares);
+    const particulares = parseInt(element.particulares.split(".").join(""));
     if (particulares && !Number.isNaN(particulares)) {
       total3 += particulares;
     } else {
@@ -40,21 +43,22 @@ const ejercicio1 = () => {
   console.timeEnd("EJERCICIO 1 - Implementación 3");
 
   console.time("EJERCICIO 1 - Implementación 4");
+  //
   let total4 = 0;
   total4 = DATA.reduce((acum, element) => {
-    const particulares = parseInt(element.particulares);
+    const particulares = parseInt(element.particulares.split(".").join(""));
     if (particulares && !Number.isNaN(particulares)) {
       return acum + particulares;
     } else {
-      console.log(element);
       return acum;
     }
-  });
+  }, 0);
   console.timeEnd("EJERCICIO 1 - Implementación 4");
 
   console.log(total);
   console.log(total2);
   console.log(total3);
+  console.log(total4);
 };
 
 ejercicio1();
