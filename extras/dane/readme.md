42d44924bd5f3bca91183f2960e1bceb5b70fe2a
saurmo <saurmo0108@gmail.com>
2021-10-28 07:41:28 -0500

    - agrupado por departamento

--- extras/dane/readme.md
+++ extras/dane/readme.md
@@ -3,3 +3,5 @@
 1. Buscar el departamento con mayor población (total)
 2. Promedio de LEA del departamento de Antioquia
 3. Buscar el municipio con menor población LEA
+
+4. Cuales son los departamentos con mayor población particular.
--- extras/dane/router/index.router.js
+++ extras/dane/router/index.router.js
@@ -10,7 +10,17 @@ router.post("/datos-dane", (req, res) => {
     let dane_csv = req.files.dane
     let buffer = dane_csv.data
     csvToJson(buffer.toString()).then(response => {
-        res.send(response)
+        let departamentos = {}
+        for (let index = 0; index < response.length; index++) {
+            const municipio = response[index];
+            if (Array.isArray(departamentos[municipio.departamento])) {
+                departamentos[municipio.departamento].push(municipio)
+            } else {
+                departamentos[municipio.departamento] = []
+                departamentos[municipio.departamento].push(municipio)
+            }
+        }
+        res.send(departamentos)
     })
 })
 
